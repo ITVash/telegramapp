76f9fd7d6116a1f39a9b56df1c24e2283a5b63a6
ITVash <49442123+ITVash@users.noreply.github.com>
2024-10-28 21:29:18 +0300

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 
 export default function Home() {
 	const app = useContext(webAppContext)
-	if (!app.initDataUnsafe.user) {
+	if (app.initDataUnsafe && !app.initDataUnsafe.user) {
 		return <h1>Loading</h1>
 	}
 	return (
@@ -15,22 +15,14 @@ export default function Home() {
 				<div className='h-full w-full text-center pt-4'>
 					<code className=''>{app.colorScheme}</code>
 					<Image
-						src={app.initDataUnsafe.user.photo_url || ""}
-						alt={app.initDataUnsafe.user.first_name}
+						src={app.initDataUnsafe.user?.photo_url || ""}
+						alt={app.initDataUnsafe.user?.first_name || ""}
 					/>
 					<h3 className='font-bold mb-1 text-xl'>
-						Welcome {app.initDataUnsafe.user.first_name}!
+						Welcome {app.initDataUnsafe.user?.first_name}!
 					</h3>
-					<p>Ваш ИД:{app.initDataUnsafe.user.id}</p>
-					<p>{app.initDataUnsafe.user.username}</p>
-					<div className='font-medium text-sm text-center'>
-						I&apos;m Mini App for Telegram
-					</div>
-					<a
-						className='mt-6 block text-lg text-cyan-500 font-bold'
-						href='https://t.me/thismisterit'>
-						My Telegram Channel
-					</a>
+					<p>Ваш ИД:{app.initDataUnsafe.user?.id}</p>
+					<p>{app.initDataUnsafe.user?.username}</p>
 				</div>
 			) : (
 				"loading"
