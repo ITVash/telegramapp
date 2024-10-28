f622b139f5279730a8d89f160c530b7cf0f65e51
ITVash <49442123+ITVash@users.noreply.github.com>
2024-10-28 21:24:38 +0300

    added info user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
-"use client";
+"use client"
 
-import { useContext } from "react";
-import { webAppContext } from "./context";
+import { useContext } from "react"
+import { webAppContext } from "./context"
+import Image from "next/image"
 
 export default function Home() {
-  const app = useContext(webAppContext);
-  
-  return (
-    <>
-    {app.version ? (
-      <div className="h-full w-full text-center pt-4">
-        <code className="">{app.colorScheme}</code>
-        <h3 className="font-bold mb-1 text-xl">Welcome {app.initDataUnsafe.user?.first_name}!</h3>
-        <div className="font-medium text-sm text-center">I&apos;m Mini App for Telegram</div>
-        <a className="mt-6 block text-lg text-cyan-500 font-bold" href="https://t.me/thismisterit">My Telegram Channel</a>
-      </div>
-      ) : (
-        "loading"
-      )}
-    </>
-  );
+	const app = useContext(webAppContext)
+	if (!app.initDataUnsafe.user) {
+		return <h1>Loading</h1>
+	}
+	return (
+		<>
+			{app.version ? (
+				<div className='h-full w-full text-center pt-4'>
+					<code className=''>{app.colorScheme}</code>
+					<Image
+						src={app.initDataUnsafe.user.photo_url || ""}
+						alt={app.initDataUnsafe.user.first_name}
+					/>
+					<h3 className='font-bold mb-1 text-xl'>
+						Welcome {app.initDataUnsafe.user.first_name}!
+					</h3>
+					<p>Ваш ИД:{app.initDataUnsafe.user.id}</p>
+					<p>{app.initDataUnsafe.user.username}</p>
+					<div className='font-medium text-sm text-center'>
+						I&apos;m Mini App for Telegram
+					</div>
+					<a
+						className='mt-6 block text-lg text-cyan-500 font-bold'
+						href='https://t.me/thismisterit'>
+						My Telegram Channel
+					</a>
+				</div>
+			) : (
+				"loading"
+			)}
+		</>
+	)
 }
